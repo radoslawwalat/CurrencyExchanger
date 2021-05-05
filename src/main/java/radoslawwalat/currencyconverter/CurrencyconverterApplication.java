218479597544dfa0270465a0574cd0a6a4beb802
Radoslaw Walat <radoslawwalat@gmail.com>
2021-05-05 14:58:42 +0200

    INITIAL COMMIT

---
+++ src/main/java/radoslawwalat/currencyconverter/CurrencyconverterApplication.java
@@ -0,0 +1,13 @@
+package radoslawwalat.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyconverterApplication.class, args);
+    }
+
+}
